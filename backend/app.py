0c34421602be4539362b51eab01c39f2f00f4da0
Jamie Bort <jamiebort@gmail.com>
2024-10-08 17:22:33 -0400

    Add flask_cors package to back end.

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World again!'
+    return "Hello, cross-origin-world!"
 
 @app.route('/test')
 def test_function():
